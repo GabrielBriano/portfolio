04bf2b9385c3d109b7e248accd44e618aea5523b
Gabriel Briano <137088982+GabrielBriano@users.noreply.github.com>
2024-10-18 19:07:04 -0300

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-
+const nextConfig = {
+    output: 'export',
+    images: {
+      unoptimized: true, // Para evitar otimizações de imagem que podem falhar em exportações estáticas
+    },
+    basePath: '/portfolio', // Ajusta o caminho para funcionar no GitHub Pages
+    assetPrefix: '/portfolio/', // Garante que os assets sejam carregados corretamente
+  };
+  
+  export default nextConfig;
   
 
+/* @type {import('next').NextConfig} 
+const nextConfig = {};
+
 export default nextConfig;
+*/
